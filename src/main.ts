015f3948786aafbbba4529b534c94520f168de58
mentegagorengz <maestrolengkong026@student.unsrat.ac.id>
2025-04-24 11:24:45 +0800

    update cors

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,12 @@ async function bootstrap() {
   console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
   app.enableCors({
-    origin: 'https://perpustakaan-unsrat.vercel.app/',
+    origin: 'https://perpustakaan-unsrat.vercel.app', // ✅ TANPA slash
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   });
+  
   app.use(cookieParser());
 
   await app.listen(4000);

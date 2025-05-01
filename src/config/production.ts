cafb9d7ff9e43f3c0c9d67a8a01a3597d7d7f4ad
mentegagorengz <maestrolengkong026@student.unsrat.ac.id>
2025-05-02 02:18:23 +0800

    update env

--- src/config/production.ts
+++ src/config/production.ts
@@ -1,6 +1,6 @@
 export default () => ({
     frontendUrl: process.env.FRONTEND_URL || 'https://https://perpustakaan-unsrat.vercel.app',
-    // databaseUrl: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_w0KkX7lNtzVM@ep-cool-cake-a11oexey-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
-    databaseUrl: process.env.FRONTEND_URL || 'postgresql://postgres:password@localhost:5432/perpustakaan',
+    databaseUrl: process.env.DATABASE_URL || "postgresql://neondb_owner:npg_w0KkX7lNtzVM@ep-cool-cake-a11oexey-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require"
+    // databaseUrl: process.env.FRONTEND_URL || 'postgresql://postgres:password@localhost:5432/perpustakaan',
   });
   

75d45e1ff860ba0c7de22c2375bd120ffb8261e9
mentegagorengz <maestrolengkong026@student.unsrat.ac.id>
2025-05-02 03:24:07 +0800

    main.ts

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,11 @@ async function bootstrap() {
     allowedHeaders: ['Content-Type', 'Authorization'],
   });
 
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+    next();
+  });
+
   app.use(cookieParser());
 
   await app.listen(4000);
